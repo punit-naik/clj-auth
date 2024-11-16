292073da026d467c78643b57a3578f33b9c61881
Punit Naik <naik.punit44@gmail.com>
2024-11-16 14:53:49 +0530

    added db utils

---
+++ src/clj_auth/backend/db.clj
@@ -0,0 +1,43 @@
+(ns clj-auth.backend.db
+  (:require [next.jdbc :as jdbc]
+            [honey.sql :as sql]
+            [clj-auth.backend.db.utils :as db.utils]))
+
+(def db-spec
+  {:dbtype "postgresql"
+   :dbname "clj_auth"
+   :host "localhost"
+   :port 5438
+   :user "clj_auth"
+   :password "clj_auth_pass"})
+
+(def datasource (jdbc/get-datasource db-spec))
+
+(defn create-users-table! []
+  (db.utils/execute!
+   datasource
+   {:create-table [:users :if-not-exists]
+    :with-columns
+    [[:id :serial [:primary-key]]
+     [:username [:varchar 255] [:not nil] [:unique]]
+     [:password [:varchar 255] [:not nil]]
+     [:created_at :timestamp [:default [:raw "CURRENT_TIMESTAMP"]]]]}))
+
+(defn get-user-by-username [username]
+  (db.utils/query-one
+   datasource
+   {:select [:*]
+    :from [:users]
+    :where [:= :username username]}))
+
+(defn create-user! [username password]
+  (try
+    (db.utils/insert!
+     datasource
+     :users
+     {:username username
+      :password password})
+    (catch Exception e
+      (if (= "23505" (.getSQLState e))
+        {:error "Username already exists"}
+        {:error (.getMessage e)}))))
---
+++ src/clj_auth/backend/db/utils.clj
@@ -0,0 +1,60 @@
+(ns clj-auth.backend.db.utils
+  (:require [honey.sql :as sql]
+            [next.jdbc :as jdbc]))
+
+(def default-opts
+  {:return-keys true
+   :builder-fn jdbc/unqualified-snake-kebab-opts})
+
+(defn format-sql
+  "Format HoneySQL query with default options"
+  [query]
+  (sql/format query {:quoted true}))
+
+(defn execute-one!
+  "Execute a query and return a single result"
+  [datasource query]
+  (jdbc/execute-one! datasource (format-sql query) default-opts))
+
+(defn execute!
+  "Execute a query and return all results"
+  [datasource query]
+  (jdbc/execute! datasource (format-sql query) default-opts))
+
+(defn insert!
+  "Insert a record and return the inserted row"
+  [datasource table row]
+  (execute-one! 
+   datasource
+   {:insert-into table
+    :values [row]
+    :returning :*}))
+
+(defn update!
+  "Update records and return updated rows"
+  [datasource table set-map where-clause]
+  (execute!
+   datasource
+   {:update table
+    :set set-map
+    :where where-clause
+    :returning :*}))
+
+(defn delete!
+  "Delete records and return deleted rows"
+  [datasource table where-clause]
+  (execute!
+   datasource
+   {:delete-from table
+    :where where-clause
+    :returning :*}))
+
+(defn query
+  "Execute a SELECT query"
+  [datasource query-map]
+  (execute! datasource query-map))
+
+(defn query-one
+  "Execute a SELECT query and return first result"
+  [datasource query-map]
+  (execute-one! datasource query-map))
